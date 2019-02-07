b1a3abc869862caaa99739ff4df161aa8ece4b89
markieeee <marcus.d.ellison@gmail.com>
2019-02-06 22:21:20 -0600

    added zeppelin, cylinder, floatydock, and goodiwifi

--- update.sh
+++ update.sh
@@ -4,3 +4,7 @@ dpkg -b projects/Twig debs/
 dpkg -b projects/pulltorespring debs/
 dpkg -b projects/snowboard debs/
 dpkg -b projects/smallsiri debs/
+dpkg -b projects/cylinder debs/
+dpkg -b projects/zeppelin debs/
+dpkg -b projects/goodwifi debs/
+dpkg -b projects/floatydock debs/
